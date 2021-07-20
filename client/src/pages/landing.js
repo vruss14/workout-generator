8d0afc4c706552777577348d8ec4ba8137214aee
Valerie Russell <vruss14@gmail.com>
2021-07-20 17:38:04 -0400

    added landing page image and set up files

---
+++ client/src/pages/landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LandingContainer from "../components/landingContainer.js"
+import LandingContent from "../components/landingContent.js"
+
+// The landing page
+
+
+function Landing(props) {
+  return (
+      <LandingContainer>
+          <LandingContent />
+      </LandingContainer>
+  );
+}
+
+export default Landing;
