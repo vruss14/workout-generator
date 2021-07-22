5917adab5bf89b8060767854ff598ab4dbe2f812
Valerie Russell <vruss14@gmail.com>
2021-07-21 20:32:20 -0400

    reorganized components; added login form

---
+++ client/src/components/loginform.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// The Log In page
+
+function LoginForm() {
+
+    // Temporary styling to ensure animation works as expected
+
+    const style = {
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: "64px",
+        marginTop: "3rem"
+    }
+
+  return (
+      <h1 style={style}>Log In</h1>
+  );
+}
+
+export default LoginForm;
