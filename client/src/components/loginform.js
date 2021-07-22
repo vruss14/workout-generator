5dd6d80d2bc471cb6efd38ac812dabdd5567cc61
Valerie Russell <vruss14@gmail.com>
2021-07-22 18:57:23 -0400

    created login form with responsive styling

--- client/src/components/loginform.js
+++ client/src/components/loginform.js
@@ -1,21 +1,35 @@
 import React from "react";
+import "./loginStyle.css";
 
 // The Log In page
 
 function LoginForm() {
 
-    // Temporary styling to ensure animation works as expected
+    return (
+        <form className="box has-background-light" id="form-container">
 
-    const style = {
-        textAlign: "center",
-        fontWeight: "bold",
-        fontSize: "64px",
-        marginTop: "3rem"
-    }
+            <h1 className="has-text-weight-bold has-text-centered mt-3 mb-3" id="login-form-title">Log In</h1>
 
-  return (
-      <h1 style={style}>Log In</h1>
-  );
+            <div className="field">
+            <label className="is-size-4 mt-4 mb-4 custom-label">Email</label>
+            <div className="control">
+                <input className="input mt-3" type="email" placeholder="e.g. alex@example.com" />
+            </div>
+            </div>
+
+            <div className="field">
+            <label className="is-size-4 mt-4 mb-4 custom-label">Password</label>
+            <div className="control">
+                <input className="input mt-3" type="password" placeholder="********" />
+            </div>
+            </div>
+
+            <button className="button is-black is-outlined is-large mt-5 has-text-centered" 
+            id="log-in-submit"
+            onClick={(event) => visitHome(event)}
+            >LOG IN</button>
+        </form>
+    )
 }
 
 export default LoginForm;
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import LoginForm from "../components/loginform.js";
+import "../components/loginStyle.css";
 
 // The Log In page
 
 function Login() {
 
+  // Switching the inline styling results in fade-in/fade-out animations
   let loginStyle = { 
     opacity: "0%", 
     transition: "1.6s linear"
@@ -14,6 +16,8 @@ function Login() {
     opacity: "100%",
     transition: "1.6s ease-in",
   }
+
+  // The useEffect hook is used to switch styles on page load, resulting in the animation
 
   useEffect(() => {
       animatePage();
@@ -25,16 +29,46 @@ function Login() {
       setAnimate(true);
   }
 
+  // Resets animate back to the default style (goes from 100% opacity to 0%)
+
   window.addEventListener('beforeunload', function (e) {
     e.preventDefault();
     setAnimate(false)
   });
 
+  function visitHome(event) {
+    event.preventDefault();
+    location.href = "/";
+  }
+
   return (
-      <div style={animate ? loginAnimated : loginStyle}>
+      <main id="main-login-container" style={animate ? loginAnimated : loginStyle}>
         <LoginForm />
-        <a href="/"><button>RETURN HOME</button></a>
-      </div>
+
+        {/* <form className="box has-background-light" id="form-container">
+
+          <h1 className="has-text-weight-bold has-text-centered mt-3 mb-3" id="login-form-title">Log In</h1>
+
+          <div className="field">
+            <label className="is-size-4 mt-4 mb-4 custom-label">Email</label>
+            <div className="control">
+              <input className="input mt-3" type="email" placeholder="e.g. alex@example.com" />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="is-size-4 mt-4 mb-4 custom-label">Password</label>
+            <div className="control">
+              <input className="input mt-3" type="password" placeholder="********" />
+            </div>
+          </div>
+
+          <button className="button is-black is-outlined is-large mt-5 has-text-centered" 
+            id="log-in-submit"
+            onClick={(event) => visitHome(event)}
+            >LOG IN</button>
+        </form> */}
+      </main>
   );
 }
 
