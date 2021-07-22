aa0a62f2cbd25959b992d17197a7a833771d0ffe
Valerie Russell <vruss14@gmail.com>
2021-07-22 18:57:08 -0400

    updated landing page description

--- client/src/components/landingContent.js
+++ client/src/components/landingContent.js
@@ -12,7 +12,7 @@ function LandingContainer() {
   return (
       <section className="is-flex is-flex-wrap-wrap" id="landing-content">
             <h1 className="is-size-1 has-text-weight-bold is-flex mt-3" id="landing-title">Workout Generator</h1>
-            <h2 className="is-size-4 mt-3 mb-3">
+            <h2 className="is-size-4 mt-3 mb-3" id="landing-description">
                 A simple solution for building healthy exercise habits. 
                 Ten minute workouts for all skill levels.</h2>
             <button className="button is-black is-outlined is-large" id="get-started-btn">GET STARTED</button>
