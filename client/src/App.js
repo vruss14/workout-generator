efa590e87466251276daff29ced124f3956c3a0b
Valerie Russell <vruss14@gmail.com>
2021-07-21 20:31:08 -0400

    implemented react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import Landing from "./pages/landing.js"
+import Landing from "./pages/landing.js";
+import Login from "./pages/login.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+
   return (
-    <Landing />
+    <Router>
+      <Switch>
+
+        <Route exact path="/">
+          <Landing />
+        </Route>
+
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+        {/* <Route>
+          <PageNotFound />
+          <Footer />
+        </Route> */}
+
+      </Switch>
+  </Router>
   );
 }
 
