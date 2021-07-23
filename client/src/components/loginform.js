66f08bc85225d03dd08774ff549766ac22eb1f8a
Valerie Russell <vruss14@gmail.com>
2021-07-22 21:00:23 -0400

    added link to create account page

--- client/src/components/loginform.js
+++ client/src/components/loginform.js
@@ -13,12 +13,14 @@ function LoginForm() {
     return (
         <form className="box has-background-light" id="form-container">
 
-            <h1 className="has-text-weight-bold has-text-centered mt-3 mb-3" id="login-form-title">Log In</h1>
+            <h1 className="has-text-weight-bold has-text-centered mt-3 mb-1" id="login-form-title">Log In</h1>
+
+            <p className="is-size-6 mb-3 has-text-centered">Need to create an account? Click <a href="/create-account">HERE</a>.</p>
 
             <div className="field">
-            <label className="is-size-4 mt-4 mb-4 custom-label">Email</label>
+            <label className="is-size-4 mt-4 mb-4 custom-label">Email/Username</label>
             <div className="control">
-                <input className="input mt-3" type="email" placeholder="e.g. alex@example.com" />
+                <input className="input mt-3" type="email" />
             </div>
             </div>
 
@@ -33,6 +35,9 @@ function LoginForm() {
             id="log-in-submit"
             onClick={(event) => visitHome(event)}
             >LOG IN</button>
+
+            <p className="is-size-6 mt-3 mb-3">Forgot your username or password? Click <a href="#">HERE</a>.</p>
+
         </form>
     )
 }
