f0175f5d56c03973ac473b7ce45f5ee6c11e150f
Valerie Russell <vruss14@gmail.com>
2021-08-20 14:01:46 -0400

    updated FAQ to use props & adjusted styling

--- client/src/components/singleFAQ.js
+++ client/src/components/singleFAQ.js
@@ -5,7 +5,7 @@ import "../css/FAQ.css";
 
 // Single question & answer 
 
-function Question() {
+function Question(props) {
 
     const [icon, setIcon] = useState(true)
 
@@ -23,12 +23,10 @@ function Question() {
         <p onClick={ () => toggleIcon()} className="question">
             <span>{icon === true ? <FontAwesomeIcon  onClick={ () => toggleIcon()} icon="plus" /> 
             : <FontAwesomeIcon onClick={() => toggleIcon()} icon="minus" /> }
-            </span> &nbsp; Does this workout generator include options for all skill levels?
+            </span> &nbsp; {props.question}
         </p>
 
-        {icon === false ? <p className="answer mt-4">Yes, all workouts are completely customizable. You can choose a gentle, moderate, or advanced workout and
-        then choose specific exercises that you would feel comfortable doing so that your generated workout doesn't have anything
-        you do not like.</p> : null }
+        {icon === false ? <p className="answer mt-4">{props.answer}</p> : null }
     </div>
   );
 }
--- client/src/pages/FAQ.js
+++ client/src/pages/FAQ.js
@@ -43,14 +43,31 @@ function FAQ() {
                 <div className="column has-background-light is-two-thirds" id="light-container">
                     <h1 id="faq-title">FREQENTLY ASKED QUESTIONS</h1>
                     <div id="questions-container">
-                        <Question />
-                        <Question />
-                        <Question />
-                        <Question />
-                        <Question />
+                        <Question
+                        question="How gentle are the gentle exercises?"
+                        answer="A gentle workout incorporates only low-impact exercises and will include time to rest.
+                        If there are exercises you don't feel comfortable doing, you can choose not to select them as you
+                        share your preferences." />
+                        <Question
+                        question="Do I need to purchase any equipment?"
+                        answer="Some of the exercises will require equipment. If you prefer exercises without equipment,
+                        select those options as you create your workout." />
+                        <Question
+                        question="How does the timer work?"
+                        answer="Once you press the start button, the timer will count down 10 times from 60 seconds.
+                        Currently, you cannot pause or reset the timer." />
+                        <Question
+                        question="What should I do if I don't understand how to perform a certain exercise?"
+                        answer="While this site currently does not have informative videos for each exercise,
+                        you are welcome to search the name of the exercise in a search engine to find demonstration videos." />
+                        <Question
+                        question="What if I want a workout that is longer than 10 minutes?"
+                        answer="You are welcome to complete your workout multiple times (e.g. repeat twice for 20 minutes,
+                        or three times for 30 minutes), or you could create multiple workouts based on your preferences." />
 
-                        <button className="button is-black is-outlined is-large" id="start-btn"
-                    onClick={() => visitGetStarted()}>GET STARTED</button>
+                        <button className="button is-black is-outlined is-large mb-2" id="start-btn"
+                        onClick={() => visitGetStarted()}>GET STARTED</button>
+
                     </div>
 
 
